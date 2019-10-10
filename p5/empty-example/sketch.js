ea81fe3db4bb6de79d9d4dc0a85a13506c023764
Alex Neff <ajneff@ilstu.edu>
2019-10-10 14:23:39 -0500

    a

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,34 +1,36 @@
-var cars [] ;
+var cars[];
 
 function setup() {
   createCanvas(800, 800);
-  for (var i = 0; i < 40; i++ ) {
-    cars.push(new Car()) ;
-}
+  for (var i = 0; i < 40; i++) {
+    cars.push(new Car());
+  }
 }
 
 function draw() {
   background(100);
-  car.display();
-  car.drive();
+  for (var i = 0; i < cars.length; i++) {
+    cars[i].display();
+    cars[i].drive();
+  }
 }
 
 function Car() {
-  this.pos = createVector(100, 100) ;
-  this.vel = createVector(random(-5, 5), random(-5,5)) ;
-  this.x = random(width) ;
+  this.pos = createVector(100, 100);
+  this.vel = createVector(random(-5, 5), random(-5, 5));
+  this.x = random(width);
 
   this.display = function() {
-    fill(this.r, this.g, this.b) ;
-    rect(this.pos.x, this.pos.y, 100, 50) ;
+    fill(this.r, this.g, this.b);
+    rect(this.pos.x, this.pos.y, 100, 50);
   }
   this.drive = function() {
-    this.pos.add(this.vel) ;
+    this.pos.add(this.vel);
 
-    if (this.pos.x > width) this.pos.x = 0 ;
-    if (this.pos.x < 0) this.pos.x = width ;
-    if (this.pos.x > height) this.pos.x = 0 ;
-    if (this.pos.x < 0) this.pos.y = height ;
+    if (this.pos.x > width) this.pos.x = 0;
+    if (this.pos.x < 0) this.pos.x = width;
+    if (this.pos.x > height) this.pos.x = 0;
+    if (this.pos.x < 0) this.pos.y = height;
   }
 
 }
