facf8e8db9cbca98f5e8bf332d83d575610b0aa2
Alex Neff <ajneff@ilstu.edu>
2019-10-10 13:46:41 -0500

    plz

---
+++ p5/week8oop1/sketch.js
@@ -0,0 +1,49 @@
+var myCar1 ; // declare
+var myCar2;
+
+function setup() {
+  createCanvas(800, 800) ;
+  myCar1 = new Car()
+  myCar2 = new Car()
+}
+
+function draw() {
+background(100) ;
+
+myCar1.display() ;
+myCar1.drive() ;
+
+myCar2.display() ;
+myCar2.drive() ;
+
+}
+
+function Car() {
+//attributes
+this.x = random(width);
+this.y = random(height);
+this.r = random(255) ;
+this.g = random(255) ;
+this.b = random(255) ;
+this.vel = random(5, 10) ;
+
+//methods
+this.display = function() {
+  fill(this.r, this.g, this.b) ;
+  rect(this.x, this.y, 100, 50);
+
+}
+
+this.drive = function() {
+  this.x = this.x +this.vel ;
+    if (this.x > width) {
+this.x = 0;
+
+  }
+
+  
+
+
+rect(this.x, 100, 150, 50) ;
+}
+}
