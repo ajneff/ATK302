c79784566b086b847bb4cec628f239812b0ef40a
Alex Neff <ajneff@ilstu.edu>
2019-10-10 13:59:57 -0500

    sk

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,92 +1,26 @@
-var myState= 1
-var timer= 0
+var car;
 
 function setup() {
-  createCanvas(1000, 1000);
-  rectMode(CENTER) ;
+  createCanvas(800, 800):
+    car = new Car();
 }
 
 function draw() {
-
-
-switch (myState) {
-
-  case 1:
-  fill (255) ;
-  rect(width/2, height/2, 140, 380) ;
-  fill(255, 255, 204) ;
-  ellipse(width/2, height/2, 100, 100) ;
-
-  push() ;
-  translate(0,- 120) ;
-  fill('red') ;
-ellipse(width/2, height/2, 100, 100) ;
-pop() ;
-
-push() ;
-translate(0, 120) ;
-fill(255) ;
-ellipse(width/2, height/2, 100, 100) ;
-pop() ;
-
-timer++ ;
-if (timer > 5*frameRate()){
-  myState = 2 ;
-  timer = 0 ;
+  background(100);
+  car.display();
+  car.drive();
 }
 
-  break;
+function Car() {
+  this.pos = createVector(100, 100) ;
+  this.x = random(width) ;
 
-  case 2:
-  fill (255) ;
-  rect(width/2, height/2, 140, 380) ;
-  fill(255, 255, 204) ;
-  ellipse(width/2, height/2, 100, 100) ;
+  this.display = function() {
+    rect(this.x, 100, 100, 50) ;
+  }
+  this.drive = function() {
+    this.x = this.x + 5;
+    if (this.x > width) this.x = 0 ;
+  }
 
-  push() ;
-  translate(0,- 120) ;
-  fill(255) ;
-ellipse(width/2, height/2, 100, 100) ;
-pop() ;
-
-push() ;
-translate(0, 120) ;
-fill('green') ;
-ellipse(width/2, height/2, 100, 100) ;
-pop() ;
-
-timer++ ;
-if (timer > 5*frameRate()){
-  myState = 3 ;
-  timer = 0 ;
-}
-
-break;
-
-
-  case 3:
-  fill (220, 210, 27) ;
-  rect(width/2, height/2, 140, 380) ;
-  fill(255, 255, 204) ;
-  ellipse(width/2, height/2, 100, 100) ;
-
-  push() ;
-  translate(0,- 120) ;
-  fill(255) ;
-ellipse(width/2, height/2, 100, 100) ;
-pop() ;
-
-push() ;
-translate(0, 120) ;
-fill(255) ;
-ellipse(width/2, height/2, 100, 100) ;
-pop() ;
-
-timer++ ;
-if (timer > 5*frameRate){
-  myState = 1 ;
-  timer = 0 ;
-}
-break;
-}
 }
