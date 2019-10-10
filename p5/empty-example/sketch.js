169ad21071d43b4988f70e9af3cbfb1fefac777f
Alex Neff <ajneff@ilstu.edu>
2019-10-10 14:20:27 -0500

    a

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,8 +1,10 @@
-var car;
+var cars [] ;
 
 function setup() {
   createCanvas(800, 800);
-    car = new Car();
+  for (var i = 0; i < 40; i++ ) {
+    cars.push(new Car()) ;
+}
 }
 
 function draw() {
@@ -13,18 +15,20 @@ function draw() {
 
 function Car() {
   this.pos = createVector(100, 100) ;
-  this.vel = createVector(random(-5, 5), random(-5,5)) ; ;
+  this.vel = createVector(random(-5, 5), random(-5,5)) ;
   this.x = random(width) ;
 
   this.display = function() {
+    fill(this.r, this.g, this.b) ;
     rect(this.pos.x, this.pos.y, 100, 50) ;
   }
   this.drive = function() {
-    this.pos.add (this.vel) ;
+    this.pos.add(this.vel) ;
+
     if (this.pos.x > width) this.pos.x = 0 ;
     if (this.pos.x < 0) this.pos.x = width ;
     if (this.pos.x > height) this.pos.x = 0 ;
-    if (this.pos.x > 0) this.pos.y = height ;
+    if (this.pos.x < 0) this.pos.y = height ;
   }
 
 }
