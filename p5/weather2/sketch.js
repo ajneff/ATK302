7a46d03a67d23ce2e97a3726f7a92696490b808a
Alex Neff <ajneff@ilstu.edu>
2019-11-10 23:37:30 -0600

    w

--- p5/weather2/sketch.js
+++ p5/weather2/sketch.js
@@ -4,7 +4,7 @@ var weatherID = 0; // returned in the JSON weather element
 var myState = 0;
 var x = 0;
 var ws = 0;
-var temperature = 0 ;
+var temperature = 0;
 let snowflakes = [];
 
 
@@ -13,7 +13,7 @@ function preload() {
   song1 = loadSound('assets/cold.mp3');
 
   song1.loop();
-  song1.stop();
+  // song1.stop();
 }
 
 
@@ -38,8 +38,8 @@ function gotData(data) {
 
   weather = data;
   console.log(data); // for debugging purposes, print out the JSON data when we get it.
-  ws = weather.wind.speed ;
-  temperature = weather.main.temp ;
+  ws = weather.wind.speed;
+  temperature = weather.main.temp;
 
 }
 
@@ -55,74 +55,75 @@ function draw() {
     case 1:
       background(200);
       scale(1.5); {
-      fill('blue');
-      text("Illinois, " + weather.name, 20, 20);
-      text("Temperature = " + weather.main.temp, 20, 40);
-      text("Humidity = " + weather.main.humidity, 20, 60);
-      text("Wind = " + weather.wind.speed, 20, 80);
-    }
+        fill('blue');
+        text("Illinois, " + weather.name, 20, 20);
+        text("Temperature = " + weather.main.temp, 20, 40);
+        text("Humidity = " + weather.main.humidity, 20, 60);
+        text("Wind = " + weather.wind.speed, 20, 80);
+      }
 
       // cloud
       fill('white');
       noStroke();
       ellipse(x, 300, 225, 170);
-      ellipse(x-100, 325, 400, 150);
-      ellipse(x-425, 200, 200, 125);
-      ellipse(x+225, 350, 200, 150);
+      ellipse(x - 100, 325, 400, 150);
+      ellipse(x - 425, 200, 200, 125);
+      ellipse(x + 225, 350, 200, 150);
 
-      x = x + ws/3;
+      x = x + ws / 3;
       if (x > width) x = 0;
 
       // thermometer
-      fill('white') ;
-      var mappedTemp = 0 ;
-      mappedTemp = map(temperature, -10, 100, 0, height) ;
-      rect(width-30, height-10, 20, mappedTemp) ;
+      fill('white');
+      var mappedTemp = 0;
+      mappedTemp = map(temperature, -10, 100, 0, height);
+      rect(width - 30, height - 10, 20, mappedTemp);
 
       break;
   }
 
   let t = frameCount / 60; // update time
 
-   // create a random number of snowflakes each frame
-   for (let i = 0; i < random(5); i++) {
-     snowflakes.push(new snowflake()); // append snowflake object
-   }
+  // create a random number of snowflakes each frame
+  for (let i = 0; i < random(5); i++) {
+    snowflakes.push(new snowflake()); // append snowflake object
+  }
 
-   // loop through snowflakes with a for..of loop
-   for (let flake of snowflakes) {
-     flake.update(t); // update snowflake position
-     flake.display(); // draw snowflake
-   }
- }
- function snowflake() {
-   // initialize coordinates
-   this.posX = 0;
-   this.posY = random(-50, 0);
-   this.initialangle = random(0, 2 * PI);
-   this.size = random(2, 5);
+  // loop through snowflakes with a for..of loop
+  for (let flake of snowflakes) {
+    flake.update(t); // update snowflake position
+    flake.display(); // draw snowflake
+  }
+}
 
-   // radius of snowflake spiral
-   // chosen so the snowflakes are uniformly spread out in area
-   this.radius = sqrt(random(pow(width / 2, 2)));
+function snowflake() {
+  // initialize coordinates
+  this.posX = 0;
+  this.posY = random(-50, 0);
+  this.initialangle = random(0, 2 * PI);
+  this.size = random(2, 5);
 
-   this.update = function(time) {
-     // x position follows a circle
-     let w = 0.6; // angular speed
-     let angle = w * time + this.initialangle;
-     this.posX = width / 2 + this.radius * sin(angle);
+  // radius of snowflake spiral
+  // chosen so the snowflakes are uniformly spread out in area
+  this.radius = sqrt(random(pow(width / 2, 2)));
 
-     // different size snowflakes fall at slightly different y speeds
-     this.posY += pow(this.size, 0.5);
+  this.update = function(time) {
+    // x position follows a circle
+    let w = 0.6; // angular speed
+    let angle = w * time + this.initialangle;
+    this.posX = width / 2 + this.radius * sin(angle);
 
-     // delete snowflake if past end of screen
-     if (this.posY > height) {
-       let index = snowflakes.indexOf(this);
-       snowflakes.splice(index, 1);
-     }
-   };
+    // different size snowflakes fall at slightly different y speeds
+    this.posY += pow(this.size, 0.5);
 
-   this.display = function() {
-     ellipse(this.posX, this.posY, this.size);
-   };
- }
+    // delete snowflake if past end of screen
+    if (this.posY > height) {
+      let index = snowflakes.indexOf(this);
+      snowflakes.splice(index, 1);
+    }
+  };
+
+  this.display = function() {
+    ellipse(this.posX, this.posY, this.size);
+  };
+}
