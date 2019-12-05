058391052a63e6a4e19564279fa35611c9537c89
Alex Neff <ajneff@ilstu.edu>
2019-12-05 14:33:09 -0600

    a

--- p5/mp11/sketch.js
+++ p5/mp11/sketch.js
@@ -8,6 +8,7 @@ function draw() {
 
     background(255) ;
     text("The Dirty bubble has aged quite a lot after retiring from the hit TV series; SpongeBob. He still urks Bikini Bottom in search to get his revenge on Mermaid Man and Barnacle Boy one day.")
+text("Time Spent: 2 hours", 20, 70);
    text(mouseX + ", " + mouseY, 20, 20) ;
    fill(255,248,220);
     ellipse(500, 500, 800, 800);
@@ -16,7 +17,7 @@ function draw() {
    triangle(85, 80, 180, 195, 245, 80);
    triangle(550, 80, 470, 195, 400, 80);
    arc(360, 515, 250, 250, 0, PI + QUARTER_PI, CHORD);
-   text("The Dirty bubble has aged quite a lot after retiring from the hit TV series; SpongeBob. He still lurks Bikini Bottom in search to get his revenge on Barnacle Boy one day.", 20, 60)
+   text("An abstract praying mantis. The rarest mantis in the world, sits and waits to feast on his next meal passing by. At the right time, (or when mouseIsPressed) the abstract praying mantis will lunge with its powerful hind legs and attack its prey with razor shar pincers.", 20, 60)
 
 
  }
